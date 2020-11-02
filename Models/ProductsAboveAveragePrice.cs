cc8dfd037961498ddee87caa5ca67e18d9264067
MaxFerraz <maxferrazrs@hotmail.com>
2020-11-02 20:17:20 -0300

    primeiro commit

---
+++ Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspCoreTeste31.Models
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
---
+++ Models/SalesByCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspCoreTeste31.Models
+{
+    public partial class SalesByCategory
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal? ProductSales { get; set; }
+    }
+}
